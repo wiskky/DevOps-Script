241448acf66f05ca6cc4ac5a0c03177a4ea7dc6e
wiskky <adeniyi.wisdom@gmail.com>
2024-11-19 04:32:41 +0100

    push first file

---
+++ firstfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "This is the user that run this program $(whoami)"
+
+#To print the system archicteture
+uname -r
+
+#Declaring a string for MYSTR
+MYSTR="WELCOME TO THE SCRIPTING CLASS"
+
+echo ${MYSTR}
+
+#Replacing THE with the word MY
+echo ${MYSTR/THE/MY}
+
+#Printing the first 10 words from MYSTR
+echo ${MYSTR::10}
+
+#Printing statrting from 12th word to the 30th word 
+echo ${MYSTR:11:30}
+
+#Printing the hostname of the server
+echo "The host name is $(HOSTNAME)"
---
+++ inte.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#To display today's time
+echo "Today's time and date is 
+
+$(date)
+"
+
+echo "This is the host file of the system"
+cat /etc/hosts
